72e1a274983548c1ec107b5d4a1e336954232249
Николай Стеклов <stenik@yandex.ru>
2026-03-16 21:09:54 +0300

    analyzer

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/service/SnapshotProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/service/SnapshotProcessor.java
@@ -157,9 +157,10 @@ public class SnapshotProcessor {
                             .filter(curAction -> curAction.getKey().equals(entry.getKey()))
                             .map(curAction -> curAction.getValue())
                             .findFirst();
-                    log.info("Все действия " +scenario.getActions().entrySet());
-                    log.info("Все условия " +scenario.getConditions().entrySet());
-                    log.info("Ключ устройства " +entry.getKey());
+                    log.info("Все действия ключи " +  scenario.getActions().keySet());
+                    log.info("Все действия " + scenario.getActions().values());
+                    log.info("Все условия ключи " + scenario.getConditions().keySet());
+                    log.info("Все условия " + scenario.getConditions().values());
                     if (optAction.isPresent()) {
                         action =  optAction.get();
                         log.info("Получили действие " + action);
